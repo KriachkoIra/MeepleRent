cb0777f06bff5edeb7573a2ef6d6f8628289237a
Iryna Kriachko <kriachkoira@gmail.com>
2025-04-10 22:44:47 +0200

    Booking confirmation

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createBooking, getUserBookings, cancelBooking } from "../controllers/booking.controller.js";
+import {
+  createBooking,
+  getUserBookings,
+  cancelBooking,
+  confirmBooking,
+} from "../controllers/booking.controller.js";
 
 const router = express.Router();
 
 router.route("/").post(createBooking).get(getUserBookings);
+router.route("/:id").post(confirmBooking);
 router.route("/:id").delete(cancelBooking);
 
 export default router;
